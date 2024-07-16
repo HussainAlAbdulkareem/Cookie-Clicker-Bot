91653597a18fb5684465a5af0484280f133a8679
HussainAlAbdulkareem <hussain.alabdulkaree@ucalgary.ca>
2024-07-16 15:17:55 +0300

    Grabs number of cookies

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ driver = webdriver.Chrome(options=chrome_options)
 driver.get("https://orteil.dashnet.org/cookieclicker/")
 
 #Wait for website to load
-time.sleep(10)
+time.sleep(8)
 
 #Selects English
 driver.find_element(By.XPATH, value='//*[@id="langSelect-EN"]').click()
@@ -17,6 +17,7 @@ driver.find_element(By.XPATH, value='//*[@id="langSelect-EN"]').click()
 time.sleep(5)
 
 cookie_button = driver.find_element(By.ID, value="bigCookie")
+cookie_button.click()
 
-while True:
-    cookie_button.click()
+num_of_cookies = int(driver.find_element(By.ID, value="cookies").text.split(" ")[0])
+print(num_of_cookies)
