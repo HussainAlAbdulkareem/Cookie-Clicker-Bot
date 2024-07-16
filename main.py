99cb617ab2fd2c2aa4159af046abb7b0b13b62f6
HussainAlAbdulkareem <hussain.alabdulkaree@ucalgary.ca>
2024-07-16 15:06:18 +0300

    Clicks on Cookie

--- main.py
+++ main.py
@@ -8,9 +8,15 @@ chrome_options.add_experimental_option("detach", True)
 driver = webdriver.Chrome(options=chrome_options)
 driver.get("https://orteil.dashnet.org/cookieclicker/")
 
-
+#Wait for website to load
 time.sleep(10)
-cookie = driver.find_element(By.XPATH, value='//*[@id="langSelect-EN"]')
 
+#Selects English
+driver.find_element(By.XPATH, value='//*[@id="langSelect-EN"]').click()
 
-cookie.click()
+time.sleep(5)
+
+cookie_button = driver.find_element(By.ID, value="bigCookie")
+
+while True:
+    cookie_button.click()
